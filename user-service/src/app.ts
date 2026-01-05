437ff6d0b72fc8eeef9433b64dd53a744aa0e9e1
Amal Thomas <amalthomas333444@gmail.com>
2026-01-05 19:12:05 +0530

    updated url1234

--- user-service/src/app.ts
+++ user-service/src/app.ts
@@ -33,12 +33,7 @@ app.use(cors({
 }));
 
 // Handle preflight requests globally
-app.options("*", cors({
-  origin: allowedOrigins,
-  credentials: true,
-  methods: ["GET","POST","PUT","DELETE","OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"]
-}));
+
 // VERY IMPORTANT
 
 app.use(express.json());
